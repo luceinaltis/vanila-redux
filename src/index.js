67ea182d071091d0cecb667ad58bc1b6e2ee7bec
Ki Hwan <luceinaltis2020@naver.com>
2020-10-07 14:20:46 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+let count = 0;
+
+number.innerText = count;
+
+const updateText = () => {
+  number.innerText = count;
+}
+
+const handleAdd = () => {
+  count++;
+  updateText();
+}
+
+const handleMinus = () => {
+  count--;
+  updateText();
+}
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
